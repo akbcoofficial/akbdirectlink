ccc9d5e04b98dbde5a89af364dc2736082335160
akbcoofficial <84227578+akbcoofficial@users.noreply.github.com>
2021-05-17 10:57:10 +0530

    Add files via upload

---
+++ searchpage.php
@@ -0,0 +1,73 @@
+<!-- Project Information
+
+~~>Name:- Akb Direct Links
+~~>Description:- Stop using search engine.Get instant 
+link with Akb Direct link.
+~~>Publisher:- Akb Co(akb.42web.io)
+~~>Creator:- Ashishdeep khokher
+-->
+<!--------------------------------------------------------------------------->
+ <!-- Php Starts-->
+	     <!--Change if you know-->
+	         <!--Changing this can cause breakup or error in website-->
+	             <!-- Watch how to change:-  http://akb.42web.io/directlink/help -->
+	                  <!--Hope this project will help you in many ways-->
+<?php 
+ 
+$result= $_POST['search'];
+$result= strtolower($result);
+
+if ($result=="dropbox")
+header ('Location: http://www.dropbox.com');
+if ($result=="dropbox website")
+header ('Location: http://www.dropbox.com');
+if ($result=="dropbox link")
+header ('Location: http://www.dropbox.com');
+if ($result=="what is dropbox")
+header ('Location: http://www.dropbox.com');
+if ($result=="online file stroage")
+header ('Location: http://www.dropbox.com');
+
+
+
+
+
+if ($result=="amazon")
+header ('Location: http://www.amazon.com');
+if ($result=="amazon india")
+header ('Location: http://www.amazon.com');
+if ($result=="amazon website")
+header ('Location: http://www.amazon.com');
+if ($result=="buy stuffs")
+header ('Location: http://www.amazon.com');
+if ($result=="Good selling Website")
+header ('Location: http://www.amazon.com');
+
+if ($result=="google")
+header ('Location: http://www.google.com');
+if ($result=="search engine")
+header ('Location: http://www.google.com');
+if ($result=="google link")
+header ('Location: http://www.google.com');
+if ($result=="google website")
+header ('Location: http://www.google.com');
+
+if ($result=="ebay")
+header ('Location: http://www.ebay.com');
+
+if ($result=="wikipedia")
+header ('Location: http://www.wikipedia.org');
+
+if ($result=="youtube")
+header ('Location: http://www.youtube.com');
+
+if ($result=="hess toy truck")
+header ('Location: https://hesstoytruck.com');
+
+if ($result=="Whatsapp")
+header ('Location: https://web.whatsapp.com');
+
+
+echo "Sorry,No results found for this search";
+?> 
+ 
